7f881cda888cdbb44ab4e7f8454ddfe3d9967ed5
Robert <40003758+rbpeng@users.noreply.github.com>
2020-02-05 13:29:13 -0600

    Add files via upload

--- project1.py
+++ project1.py
@@ -5,35 +5,36 @@ from pathlib import Path
 
 key = 'M3E8O6PCOLI682KH'
 
-def get_daily_equity_data(outdir, filename, key=key, stock='AAPL', values='all'):
+def get_daily_equity_data(outdir, filename, key=key, stock='AAPL', values='all', num_days=100):
     ts = TimeSeries(key, output_format='pandas')
-    data, meta = ts.get_daily(symbol=stock)
+    data, meta = ts.get_daily(symbol=stock, outputsize='full')
     filepath = Path(outdir, filename+'.csv')
     if values=='all':
+        data=pd.DataFrame.head(data, n=num_days)
         pd.DataFrame.to_csv(data, filepath, header=True)
         return
     elif values=='open':
-        data=data[data.columns[0]]
+        data=pd.DataFrame.head(data[data.columns[0]], n=num_days)
         pd.DataFrame.to_csv(data, filepath, header=True)
         return
     elif values=='high':
-        data=data[data.columns[1]]
+        data=pd.DataFrame.head(data[data.columns[1]], n=num_days)
         pd.DataFrame.to_csv(data, filepath, header=True)
         return
     elif values=='low':
-        data=data[data.columns[2]]
+        data = pd.DataFrame.head(data[data.columns[2]], n=num_days)
         pd.DataFrame.to_csv(data, filepath, header=True)
         return
     elif values=='close':
-        data=data[data.columns[3]]
+        data=pd.DataFrame.head(data[data.columns[3]], n=num_days)
         pd.DataFrame.to_csv(data, filepath, header=True)
         return
     elif values=='volume':
-        data=data[data.columns[4]]
+        data=pd.DataFrame.head(data[data.columns[4]], n=num_days)
         pd.DataFrame.to_csv(data, filepath, header=True)
         return
     else:
         print("Error: values must be \'all\', \'open\', \'high\', \'low\', \'close\', or \'volume\'")
         return
 
-# Example: get_daily_equity_data(outdir="C:/Users/RBP7855/Desktop", filename="test", stock='MSFT', daily='all')
+# Example: get_daily_equity_data(outdir="C:/Users/RBP7855/Desktop", filename="test", stock='MSFT', values='all'. num_days=50)

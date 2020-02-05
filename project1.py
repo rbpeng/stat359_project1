642014c2e3ebde98e3eb182aefa83ed8b95b67f2
Robert <40003758+rbpeng@users.noreply.github.com>
2020-02-05 16:11:41 -0600

    Add files via upload

--- project1.py
+++ project1.py
@@ -1,4 +1,5 @@
 from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.foreignexchange import ForeignExchange
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -37,4 +38,24 @@ def get_daily_equity_data(outdir, filename, key=key, stock='AAPL', values='all',
         print("Error: values must be \'all\', \'open\', \'high\', \'low\', \'close\', or \'volume\'")
         return
 
-# Example: get_daily_equity_data(outdir="C:/Users/RBP7855/Desktop", filename="test", stock='MSFT', values='all'. num_days=50)
+# Example: get_daily_equity_data(outdir="C:/Users/RBP7855/Desktop", filename="MSFT", stock='MSFT', values='all', num_days=500)
+
+def get_daily_currency_data(outdir, filename, key=key, curr1='EUR', curr2='USD', values='all', num_days=100):
+    fe = ForeignExchange(key, output_format='pandas')
+    data, meta = fe.get_currency_exchange_daily(from_symbol=curr1, to_symbol=curr2, outputsize='full')
+    filepath = Path(outdir, filename+'.csv')
+    if values=='all':
+        data=pd.DataFrame.head(data, n=num_days)
+        pd.DataFrame.to_csv(data, filepath, header=True)
+        return
+    options = ['open', 'high', 'low', 'close']
+    for i in range(len(options)):
+        if values == options[i]:
+            data=pd.DataFrame.head(data[data.columns[i]], n=num_days)
+            pd.DataFrame.to_csv(data, filepath, header=True)
+            return
+    else:
+        print("Error: values must be \'all\', \'open\', \'high\', \'low\', or \'close\'")
+        return
+
+# Example: get_daily_currency_data(outdir="C:/Users/RBP7855/Desktop", filename="JPY_to_USD", curr1='JPY', curr2='USD', values='open', num_days=500)
